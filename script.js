f12a12cef6ca3fbd32faf389f8b90a9d41027b69
Jlc758 <janelle.chafe@gmail.com>
2023-11-22 00:57:43 -0330

    Use JS to add delayed color change to Flag button

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const flags = document.querySelectorAll(".flag");
 
-for (const flag of flags)
-    flag.addEventListener('click', () => {
-        flag.style.backgroundColor = "black"})
+
+for (const flag of flags) 
+    flag.addEventListener ('click', () => {flag.style.backgroundColor = "#e5989b";
+    setTimeout(() => {
+        flag.style.backgroundColor = "#83c5be"
+    }, 1000);} )
+    
