b9ab8825f7aba8b3863c3c419fe417273478123f
Jlc758 <janelle.chafe@gmail.com>
2023-11-23 23:54:47 -0330

    Edit accordion element without bootstrap

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const flags = document.querySelectorAll(".flag");
-
+let accordionHeader = document.querySelector(".accordion-header")
 
 for (const flag of flags) 
     flag.addEventListener ('click', () => {flag.style.backgroundColor = "#e5989b";
@@ -8,13 +8,25 @@ for (const flag of flags)
     }, 1000);} )
 ;
 
+
+function toggleCollapse() {
+    let element = document.getElementsByClassName("accordion-header");
+    if (element.style.display === 'none'){
+        element.style.display = 'block'; 
+    }
+    else element.style.display ='none';
+    }
+
+accordionHeader.addEventListener('click', toggleCollapse);
+
+/*
 document.addEventListener('DOMContentLoaded', function () {
     let accordionItems = document.querySelector('.accordion-item');
 
-    accordionItem.forEach(function (item) {
-        let header = item.querySelector('.accordion-header');
+    accordionItems.forEach(function (item) {
+        let accordionHeader = item.querySelector('.accordion-header');
 
-        header.addEventListener('click', function () {
+        accordionHeader.addEventListener('click', function () {
             //Toggle content visibility
             let content = item.querySelector('.accordion-content');
             content.style.display = content.style.display === 'block' ? 'none' : 'block';
@@ -28,4 +40,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
         })
     })
-})
+} */
