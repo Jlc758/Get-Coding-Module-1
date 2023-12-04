e01bc6d3979346e4912ab7a33c32968790d51774
Jlc758 <janelle.chafe@gmail.com>
2023-12-04 17:41:45 -0330

    Clean up HTML semantics & nest sections under accordion parent

--- script.js
+++ script.js
@@ -1,88 +1,47 @@
 // ! I believe you had this done prior to our last session but we took it out in testing something. This is good practice to ensure your HTML is fully loaded before the script runs. This is generally more important if you have script tags in the head of your HTML. If you have script tags at the bottom of your HTML, you can usually remove this.
-document.addEventListener("DOMContentLoaded", () => {
-  const flags = document.querySelectorAll(".flag");
 
-  for (const flag of flags)
-    flag.addEventListener("click", () => {
-      flag.style.backgroundColor = "#e5989b";
-      setTimeout(() => {
-        flag.style.backgroundColor = "#83c5be";
-      }, 1000);
-    });
+const flags = document.querySelectorAll(".flag");
 
-  // ! I think it makes more sense to select the entire section not just the header
-  let accordionItems = document.querySelectorAll(".accordion-item");
-
-  accordionItems.forEach((item) => {
-    // ! Find the accordion content within the item
-    let content = item.querySelector(".accordion-content");
-
-    // ! Initialize display style
-    content.style.display = "none";
-
-    item.addEventListener("click", (event) => {
-      // ! Prevent event from bubbling up from interactive elements, otherwise clicking these inside of the section may just close the section
-      if (
-        event.target.tagName === "BUTTON" ||
-        event.target.tagName === "A" ||
-        event.target.tagName === "INPUT"
-      ) {
-        return;
-      }
-
-      // ! Toggle the display of the content
-      content.style.display =
-        content.style.display === "block" || content.style.display === ""
-          ? "none"
-          : "block";
-    });
+for (const flag of flags)
+  flag.addEventListener("click", () => {
+    flag.style.backgroundColor = "#e5989b";
+    setTimeout(() => {
+      flag.style.backgroundColor = "#83c5be";
+    }, 1000);
   });
 
-  // let accordionHeader = document.querySelectorAll(".accordion-header");
-  // let accordionContent = document.querySelectorAll(".accordion-content");
+// ! I think it makes more sense to select the entire section not just the header
+let accordionItems = document.querySelectorAll(".accordion-item");
 
-// for (const item of accordionHeader)
-//   item.addEventListener("click", () => {
-//     accordionContent.addEventListener(
-//       "change",
-//       () =>
-//         (accordionContent.style.display = "none"
-//           ? update("none", "block")
-//           : update("block", "none"))
-//     );
-//   });
-/*
-for (const item of accordionHeader)
-  item.addEventListener("click", () => {
-    accordionContent.display === "block"
-      ? (accordionContent.display = "none")
-      : (accordionContent.display = "block");
+accordionItems.forEach((item) => {
+  // ! Find the accordion content within the item
+  let content = item.querySelector(".accordion-content");
+  console.log("content", content);
 
-//     console.log("is this working??");
-//   });
+  // ! Initialize display style
+  content.style.display = "none";
 
-/*
-    accordionContent.display === "block"
-      ? (accordionContent.display = "none")
-      : (accordionContent.display = "block");
+  item.addEventListener("click", (event) => {
+    // ! Prevent event from bubbling up from interactive elements, otherwise clicking these inside of the section may just close the section
+    if (
+      event.target.tagName === "BUTTON" ||
+      event.target.tagName === "A" ||
+      event.target.tagName === "INPUT"
+    ) {
+      return;
+    }
 
-      */
-
-/*
-    item.accordionHeader.forEach(function (otherItem) {
-      if (otherItem !== item) {
-        let otherContent = otherItem.accordionContent;
-        if (otherContent.display !== "none") {
-          otherContent.display = "none";
-        }
-      } 
-    });
+    // ! Toggle the display of the content
+    content.style.display =
+      content.style.display === "block" || content.style.display === ""
+        ? "none"
+        : "block";
   });
+});
 
-let buttonSubmit = document.querySelectorAll(".button-submit");
-let fillableResults = document.getElementById("fillable").value;
-
-buttonSubmit.addEventListener("click", function () {
-  console.log("Entry", fillableResults);
-}); */
+document.getElementById("submitEntry").addEventListener("submit", (event) => {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+  let journalData = document.getElementById("fillableEntry").value;
+  console.log(journalData);
 });
