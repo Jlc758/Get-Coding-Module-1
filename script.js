3364388fd9420c601035c89c0a0c7130495c5055
apreynolds1989 <70614076+apreynolds1989@users.noreply.github.com>
2023-12-07 18:53:13 -0330

    Code Review

--- script.js
+++ script.js
@@ -10,47 +10,57 @@ for (const flag of flags)
     }, 1000);
   });
 
-// ! I think it makes more sense to select the entire section not just the header
 let accordionItems = document.querySelectorAll(".accordion-item");
 
 accordionItems.forEach((item) => {
-  // ! Find the accordion content within the item
   let content = item.querySelector(".accordion-content");
   console.log("content", content);
 
-  // ! Initialize display style
   content.style.display = "none";
 
   item.addEventListener("click", (event) => {
-    // ! Prevent event from bubbling up from interactive elements, otherwise clicking these inside of the section may just close the section
-    if (
-      event.target.tagName === "BUTTON" ||
-      event.target.tagName === "A" ||
-      event.target.tagName === "INPUT"
-    ) {
-      return;
+    //! Using closest over TagName is better because it will work even if you change the HTML structure. For example, if you changed the button to a div, this would still work. If you used tagName, it would not. The issue you were having is because your button was actually an img nested inside a button so TagName was not registering it as button. The closest() method works by looking at the element itself and then traversing up through its ancestors in the DOM tree (parents, grandparents, etc.) until it finds a node that matches the provided selector string. It returns the first matching node or null if there is no matching node.
+
+    // ! This is just a cleaner way to write the line here. Basically, if the target of the event is NOT a button, a link, or an input, then run the code.
+    if (!event.target.closest("button, a, input")) {
+      let content = item.querySelector(".accordion-content");
+      content.style.display =
+        content.style.display === "block" ? "none" : "block";
     }
-
-    content.style.display =
-      content.style.display === "block" || content.style.display === ""
-        ? "none"
-        : "block";
-
-    accordionItems.forEach();
   });
+
+  //! accordionItems.forEach();
 });
 
+//! Nice use of a function !
 function addMedication() {
   // Get the input value
-  var newMedText = document.getElementById("newMedication").value;
+  // ! var is outdated and rarely used now, use const if the variable shouldn't change or use let if it should be able to change. Generally best to use const everywhere uless you can't
+  const newMedText = document.getElementById("newMedication").value;
+
+  //! Now what if someone submits an empty string?
+
+  //! Select your medList into a variable so you don't have to keep selecting it
+  let medList = document.getElementById("medList");
+
+  //! But what if it doesn't exist ???
+
+  //! Dynamically create the 'ul' element to append your list items to, if it doesn't exist
 
   // Create a new list item
-  var newMed = document.createElement("li");
+  const newMed = document.createElement("li");
   newMed.appendChild(document.createTextNode(newMedText));
 
+  //! Make sure medList exists before appending to it, see above
   // Add the new item to the list
-  document.getElementById("medList").appendChild(newMed);
+  medList.appendChild(newMed);
 
+  //! This should clear the input field not the medlists
   // Clear the input field
   document.getElementById("medList").value = "";
 }
+
+//! Lets talk about Arrow Functions !
+//! const func = () => {} is the same as function func() {}. Arrow functions are a newer way to write functions and are generally preferred. They are also more flexible. For example, you can write const func = (param1, param2) => {} and it will work the same as function func(param1, param2) {}. The only time you can't use arrow functions is when you need to use the this keyword (don't worry about that for now).
+
+//! Add event listener here for the add medication button
