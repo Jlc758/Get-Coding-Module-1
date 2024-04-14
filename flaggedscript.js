045512dd90a22c57894114461955ed86a59ceb95
Jlc758 <janelle.chafe@gmail.com>
2024-04-14 21:25:17 -0230

    Translated selected radio for emotion and water to relevant icon on flagged entries page

--- flaggedscript.js
+++ flaggedscript.js
@@ -4,20 +4,6 @@ let timezoneOffset = currentDate.getTimezoneOffset();
 currentDate.setMinutes(currentDate.getMinutes() - timezoneOffset);
 let formattedDate = currentDate.toISOString().slice(0, 10);
 
-let newObjIsFlagged = false;
-
-let dailyEntryObj = {
-  date: formattedDate,
-  weather: "",
-  journal: "",
-  isFlagged: newObjIsFlagged,
-  emotionTracker: "",
-  waterTracker: "",
-  medications: [],
-  exercises: [],
-  habits: [],
-};
-
 const entriesKey = "entriesArray";
 const entriesArray = JSON.parse(localStorage.getItem(entriesKey));
 
@@ -33,12 +19,75 @@ flaggedEntries.forEach((entry) => {
   flaggedOther.classList = "flagged-content";
   flaggedOther.style.display = "none";
 
-  flaggedOther.textContent =
-    entry.weather +
-    entry.emotionTracker +
-    entry.medications +
-    entry.exercises +
-    entry.habits;
+  // Emotion Tracker Results
+  let selectedEmotion;
+
+  if (entry.emotionTracker === "one") {
+    selectedEmotion = document.createElement("div");
+    let resultImg = document.createElement("img");
+    resultImg.src = "./icons_images/icons8-disgusting-100.png";
+    selectedEmotion.appendChild(resultImg);
+  } else if (entry.emotionTracker === "two") {
+    selectedEmotion = document.createElement("div");
+    let resultImg = document.createElement("img");
+    resultImg.src = "./icons_images/icons8-nauseated-face-100.png";
+    selectedEmotion.appendChild(resultImg);
+  } else if (entry.emotionTracker === "three") {
+    selectedEmotion = document.createElement("div");
+    let resultImg = document.createElement("img");
+    resultImg.src = "./icons_images/icons8-pouting-face-100.png";
+    selectedEmotion.appendChild(resultImg);
+  } else if (entry.emotionTracker === "four") {
+    selectedEmotion = document.createElement("div");
+    let resultImg = document.createElement("img");
+    resultImg.src = "./icons_images/icons8-blushing-100.png";
+    selectedEmotion.appendChild(resultImg);
+  } else if (entry.emotionTracker === "five") {
+    selectedEmotion = document.createElement("div");
+    let resultImg = document.createElement("img");
+    resultImg.src =
+      "./icons_images/icons8-grinning-face-with-smiling-eyes-100.png";
+    selectedEmotion.appendChild(resultImg);
+  } else {
+    selectedEmotion = document.createElement("div");
+    selectedEmotion = "No emotion documented.";
+  }
+
+  // Water Tracker Results
+  let selectedWater;
+
+  if (entry.waterTracker === "one") {
+    selectedWater = document.createElement("div");
+    let resultImg = document.createElement("img");
+    resultImg.src = "./icons_images/icons8-water-0.png";
+    selectedWater.appendChild(resultImg);
+  } else if (entry.waterTracker === "two") {
+    selectedWater = document.createElement("div");
+    let resultImg = document.createElement("img");
+    resultImg.src = "./icons_images/icons8-water-8.png";
+    selectedWater.appendChild(resultImg);
+  } else if (entry.waterTracker === "three") {
+    selectedWater = document.createElement("div");
+    let resultImg = document.createElement("img");
+    resultImg.src = "./icons_images/icons8-water-16.png";
+    selectedWater.appendChild(resultImg);
+  } else if (entry.waterTracker === "four") {
+    selectedWater = document.createElement("div");
+    let resultImg = document.createElement("img");
+    resultImg.src = "./icons_images/icons8-water-24.png";
+    selectedWater.appendChild(resultImg);
+  } else if (entry.waterTracker === "five") {
+    selectedWater = document.createElement("div");
+    let resultImg = document.createElement("img");
+    resultImg.src = "./icons_images/icons8-water-32.png";
+    selectedWater.appendChild(resultImg);
+  } else {
+    selectedWater = document.createElement("div");
+    selectedWater = "No water documented.";
+  }
+
+  flaggedOther.append(selectedEmotion);
+  flaggedOther.append(selectedWater);
 
   flaggedJournal.append(flaggedOther);
   container.appendChild(flaggedJournal);
