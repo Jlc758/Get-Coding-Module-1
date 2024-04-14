279f81d8593ff39c3911c3217a0932520a8f3df9
Jlc758 <janelle.chafe@gmail.com>
2024-04-14 20:46:22 -0230

    Reverted back to when flaggedOther was showing.  Styling and object properties still required

--- flaggedscript.js
+++ flaggedscript.js
@@ -21,7 +21,7 @@ let dailyEntryObj = {
 const entriesKey = "entriesArray";
 const entriesArray = JSON.parse(localStorage.getItem(entriesKey));
 
-const flaggedEntries = entriesArray.map(entry.isFlagged);
+const flaggedEntries = entriesArray.filter((entry) => entry.isFlagged);
 
 const container = document.getElementById("flaggedItemsContainer");
 
@@ -33,101 +33,12 @@ flaggedEntries.forEach((entry) => {
   flaggedOther.classList = "flagged-content";
   flaggedOther.style.display = "none";
 
-  // const entryMedArray = entry.medications.map((medication) => {
-  //   const medChecked = medication.IsChecked ? "Yes" : "No";
-  //   return `Medication Name:  ${medication.MedText} Dosage:  ${medication.MedCount} Taken?:  ${medChecked}`;
-  // });
-
-  // const entryExArray = entry.exercises.map((exercise) => {
-  //   const exChecked = exercise.IsChecked ? "Yes" : "No";
-  //   return `Exercise:  ${exercise.Exercise} Reps:  ${exercise.RepCount} Completed?:  ${exChecked}`;
-  // });
-
-  // const entryHabArray = entry.habits.map((habit) => {
-  //   const habChecked = habit.IsChecked ? "Yes" : "No";
-  //   return `Habit:  ${habit.Habit} Completed?:  ${habChecked}`;
-  // });
-
-  // const savedMedString = entryMedArray.join("\n");
-  // const savedExString = entryExArray.join("\n");
-  // const savedHabString = entryHabArray.join("\n");
-
-  // const emotionOptions = document.querySelectorAll(
-  //   'input[name="emotionTracker"]'
-  // );
-
-  // const selectedEmotion = emotionOptions.querySelector(
-  //   'input[name="emotionTracker"]:checked'
-  // );
-
-  // if (selectedEmotion) {
-  //   const emotionImg = document.createElement("img");
-
-  //   switch (selectedEmotion.value) {
-  //     case "one":
-  //       emotionImg.src = "./icons_images/icons8-disgusting-50.png";
-  //       break;
-  //     case "two":
-  //       emotionImg.src = "./icons_images/icons8-nauseated-face-50.png";
-  //       break;
-  //     case "three":
-  //       emotionImg.src = "./icons_images/icons8-pouting-face-50.png";
-  //       break;
-  //     case "four":
-  //       emotionImg.src = "./icons_images/icons8-blushing-50.png";
-  //       break;
-  //     case "five":
-  //       emotionImg.src =
-  //         "./icons_images/icons8-grinning-face-with-smiling-eyes-50.png";
-  //       break;
-
-  //     default:
-  //       emotionImg.src = "";
-  //       break;
-  //   }
-  // }
-
-  // const waterOptions = document.querySelectorAll('input[name="waterTracker"]');
-
-  // flaggedOther = ` ${entry.weather}`;
-  // const emotionOptions = document.querySelectorAll(
-  //   'input[name="emotionTracker"]'
-  // );
-
-  // const selectedEmotion = emotionOptions.querySelector(
-  //   'input[name="emotionTracker"]:checked'
-  // );
-
-  // if (selectedEmotion) {
-  //   const emotionImg = document.createElement("img");
-
-  //   switch (selectedEmotion.value) {
-  //     case "one":
-  //       emotionImg.src = "./icons_images/icons8-disgusting-50.png";
-  //       break;
-  //     case "two":
-  //       emotionImg.src = "./icons_images/icons8-nauseated-face-50.png";
-  //       break;
-  //     case "three":
-  //       emotionImg.src = "./icons_images/icons8-pouting-face-50.png";
-  //       break;
-  //     case "four":
-  //       emotionImg.src = "./icons_images/icons8-blushing-50.png";
-  //       break;
-  //     case "five":
-  //       emotionImg.src =
-  //         "./icons_images/icons8-grinning-face-with-smiling-eyes-50.png";
-  //       break;
-
-  //     default:
-  //       emotionImg.src = "";
-  //       break;
-  //   }
-  // }
-  // selectedEmotion;
-  `${savedMedString}
-    ${savedExString}
-    ${savedHabString}`;
+  flaggedOther.textContent =
+    entry.weather +
+    entry.emotionTracker +
+    entry.medications +
+    entry.exercises +
+    entry.habits;
 
   flaggedJournal.append(flaggedOther);
   container.appendChild(flaggedJournal);
@@ -139,8 +50,6 @@ flaggedEntries.forEach((entry) => {
     }
   });
 });
-
-// !Go into objects
 
 // container.forEach((item) => {
 //   let content = item.querySelectorAll("flagged-content");
