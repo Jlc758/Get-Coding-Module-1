cbb2f3d2029a9772c858a6c50966c4879bd431d5
Jlc758 <janelle.chafe@gmail.com>
2024-04-20 17:19:24 -0230

    Change flaggedEntries and allEntries to cards

--- allentriesscript.js
+++ allentriesscript.js
@@ -5,137 +5,164 @@ currentDate.setMinutes(currentDate.getMinutes() - timezoneOffset);
 let formattedDate = currentDate.toISOString().slice(0, 10);
 
 const entriesKey = "entriesArray";
-const entriesArray = JSON.parse(localStorage.getItem(entriesKey));
-console.log("Entries Array: ", entriesArray);
+const entriesArray = JSON.parse(localStorage.getItem(entriesKey)) || [];
 
 const allEntries = entriesArray;
 
-const container = document.getElementById("allItemsContainer");
+const cardAccordion = document.querySelectorAll(".card-accordion");
 
-entriesArray.forEach((entry) => {
-  const journalEntry = document.createElement("div");
-  journalEntry.classList = "entry-header";
-  journalEntry.textContent = `${entry.date} // ${entry.journal}`;
+cardAccordion.forEach((card) => {
+  let content = card.querySelector(".card-content");
+  let header = card.querySelector(".card-title");
 
-  const journalOther = document.createElement("div");
-  journalOther.classList = "other-content";
-  journalOther.style.display = "none";
+  content.style.display = "none";
 
-  // Emotion Tracker Results
-  let selectedEmotion;
-
-  if (entry.emotionTracker === "one") {
-    selectedEmotion = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-disgusting-100.png";
-    selectedEmotion.appendChild(resultImg);
-  } else if (entry.emotionTracker === "two") {
-    selectedEmotion = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-nauseated-face-100.png";
-    selectedEmotion.appendChild(resultImg);
-  } else if (entry.emotionTracker === "three") {
-    selectedEmotion = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-pouting-face-100.png";
-    selectedEmotion.appendChild(resultImg);
-  } else if (entry.emotionTracker === "four") {
-    selectedEmotion = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-blushing-100.png";
-    selectedEmotion.appendChild(resultImg);
-  } else if (entry.emotionTracker === "five") {
-    selectedEmotion = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src =
-      "./icons_images/icons8-grinning-face-with-smiling-eyes-100.png";
-    selectedEmotion.appendChild(resultImg);
-  } else {
-    selectedEmotion = document.createElement("div");
-    selectedEmotion = "No emotion documented.";
-  }
-
-  // Water Tracker Results
-  let selectedWater;
-
-  if (entry.waterTracker === "one") {
-    selectedWater = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-water-0.png";
-    selectedWater.appendChild(resultImg);
-  } else if (entry.waterTracker === "two") {
-    selectedWater = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-water-8.png";
-    selectedWater.appendChild(resultImg);
-  } else if (entry.waterTracker === "three") {
-    selectedWater = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-water-16.png";
-    selectedWater.appendChild(resultImg);
-  } else if (entry.waterTracker === "four") {
-    selectedWater = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-water-24.png";
-    selectedWater.appendChild(resultImg);
-  } else if (entry.waterTracker === "five") {
-    selectedWater = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-water-32.png";
-    selectedWater.appendChild(resultImg);
-  } else {
-    selectedWater = document.createElement("div");
-    selectedWater = "No water documented.";
-  }
-
-  //   Weather Results
-
-  const weather = entry.weather;
-  const weatherDiv = document.createElement("div");
-  weatherDiv.textContent = weather;
-
-  // Medication Results
-  const medications = entry.medications;
-  const medDiv = document.createElement("div");
-  medications.forEach((medication) => {
-    const savedMedText = document.createElement("p");
-    savedMedText.textContent = `Medication: ${medication.MedText}, Count: ${medication.MedCount}, Checked: ${medication.IsChecked}`;
-    medDiv.appendChild(savedMedText);
-  });
-
-  // Exercises Results
-  const exercises = entry.exercises;
-  const exDiv = document.createElement("div");
-  exercises.forEach((exercise) => {
-    const savedExText = document.createElement("p");
-    savedExText.textContent = `Exercise: ${exercise.Exercise}, Reps: ${exercise.RepCount}, Checked: ${exercise.IsChecked}`;
-    exDiv.appendChild(savedExText);
-  });
-
-  // Habits Results
-  const habits = entry.habits;
-  const habDiv = document.createElement("div");
-  habits.forEach((habit) => {
-    const savedHabText = document.createElement("p");
-    savedHabText.textContent = `Habit: ${habit.Habit}, Checked: ${habit.IsChecked}`;
-    habDiv.appendChild(savedHabText);
-  });
-
-  // Appending All Results to journalOther
-  journalOther.append(selectedEmotion);
-  journalOther.append(selectedWater);
-  journalOther.append(weatherDiv);
-  journalOther.append(medDiv);
-  journalOther.append(exDiv);
-  journalOther.append(habDiv);
-
-  journalEntry.append(journalOther);
-  container.appendChild(journalEntry);
-
-  journalEntry.addEventListener("click", (event) => {
+  header.addEventListener("click", (event) => {
     if (!event.target.closest("button, a, input")) {
-      journalOther.style.display =
-        journalOther.style.display === "block" ? "none" : "block";
+      content.style.display =
+        content.style.display === "block" ? "none" : "block";
     }
   });
 });
+
+function createEmotionElement(emotionTracker) {
+  const selectedEmotion = document.createElement("div");
+  let emotionImg = document.createElement("img");
+
+  const emotionMap = {
+    one: "./icons_images/icons8-disgusting-100.png",
+    two: "./icons_images/icons8-nauseated-face-100.png",
+    three: "./icons_images/icons8-pouting-face-100.png",
+    four: "./icons_images/icons8-blushing-100.png",
+    five: "./icons_images/icons8-grinning-face-with-smiling-eyes-100.png",
+  };
+
+  const imgSrc = emotionMap[emotionTracker];
+
+  if (imgSrc) {
+    emotionImg.src = imgSrc;
+    selectedEmotion.appendChild(emotionImg);
+  } else {
+    selectedEmotion.textContent = "No emotion selected.";
+  }
+  return selectedEmotion;
+}
+
+function createWaterElement(waterTracker) {
+  const selectedWater = document.createElement("div");
+  let waterImg = document.createElement("img");
+
+  const waterMap = {
+    one: "./icons_images/icons8-water-0.png",
+    two: "./icons_images/icons8-water-8.png",
+    three: "./icons_images/icons8-water-16.png",
+    four: "./icons_images/icons8-water-24.png",
+    five: "./icons_images/icons8-water-32.png",
+  };
+
+  const imgSrc = waterMap[waterTracker];
+
+  if (imgSrc) {
+    waterImg.src = imgSrc;
+    selectedWater.appendChild(waterImg);
+  } else {
+    selectedWater.textContent = "Water intake not tracked.";
+  }
+  return selectedWater;
+}
+
+function createMedicationsElement(medications) {
+  const medDiv = document.createElement("div");
+
+  medications.forEach((medication) => {
+    const medicationElement = document.createElement("div");
+    medicationElement.textContent = `Medication: ${medication.MedText}, Count: ${medication.MedCount}, Checked: ${medication.IsChecked}`;
+
+    medDiv.append(medicationElement);
+  });
+  return medDiv;
+}
+
+function createExercisesElement(exercises) {
+  const exDiv = document.createElement("div");
+
+  exercises.forEach((exercise) => {
+    const exerciseElement = document.createElement("div");
+    exerciseElement.textContent = `Exercise: ${exercise.Exercise}, Reps: ${exercise.RepCount}, Checked: ${exercise.IsChecked}`;
+
+    exDiv.append(exerciseElement);
+  });
+  return exDiv;
+}
+
+function createHabitsElement(habits) {
+  const habDiv = document.createElement("div");
+
+  habits.forEach((habit) => {
+    const habitElement = document.createElement("div");
+    habitElement.textContent = `Habit: ${habit.Habit}, Checked: ${habit.IsChecked}`;
+
+    habDiv.append(habitElement);
+  });
+  return habDiv;
+}
+
+function displayCards() {
+  const cardsContainer = document.getElementById("allItemsContainer");
+  cardsContainer.innerHTML = "";
+
+  allEntries.forEach((entry) => {
+    const weather = entry.weather;
+    let emotionTracker = entry.emotionTracker;
+    let waterTracker = entry.waterTracker;
+    let medications = entry.medications;
+    let exercises = entry.exercises;
+    let habits = entry.habits;
+
+    let selectedEmotion = createEmotionElement(emotionTracker);
+    let selectedWater = createWaterElement(waterTracker);
+    let medicationList = createMedicationsElement(medications);
+    let exerciseList = createExercisesElement(exercises);
+    let habitsList = createHabitsElement(habits);
+
+    let cardContent =
+      weather +
+      selectedEmotion.outerHTML +
+      selectedWater.outerHTML +
+      medicationList.outerHTML +
+      exerciseList.outerHTML +
+      habitsList.outerHTML;
+
+    const card = document.createElement("div");
+    card.classList.add("card", "w-50", "p-3");
+
+    const cardJournal = document.createElement("p");
+    cardJournal.classList.add("card-journal");
+    cardJournal.textContent = entry.journal;
+
+    const cardAccordion = document.createElement("div");
+    cardAccordion.classList.add("card-accordion");
+
+    const cardContentDiv = document.createElement("div");
+    cardContentDiv.classList.add("card-content");
+    cardContentDiv.innerHTML = cardContent;
+
+    const cardBodyTitle = document.createElement("div");
+    cardBodyTitle.classList.add("card-body");
+
+    const cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.textContent = entry.date;
+
+    cardAccordion.appendChild(cardContentDiv);
+    cardBodyTitle.appendChild(cardTitle);
+
+    card.appendChild(cardJournal);
+    card.appendChild(cardAccordion);
+    card.appendChild(cardBodyTitle);
+
+    cardsContainer.appendChild(card);
+  });
+}
+
+displayCards();
--- flaggedscript.js
+++ flaggedscript.js
@@ -5,137 +5,164 @@ currentDate.setMinutes(currentDate.getMinutes() - timezoneOffset);
 let formattedDate = currentDate.toISOString().slice(0, 10);
 
 const entriesKey = "entriesArray";
-const entriesArray = JSON.parse(localStorage.getItem(entriesKey));
+const entriesArray = JSON.parse(localStorage.getItem(entriesKey)) || [];
 
 const flaggedEntries = entriesArray.filter((entry) => entry.isFlagged);
 
-const container = document.getElementById("flaggedItemsContainer");
+const cardAccordion = document.querySelectorAll(".card-accordion");
 
-flaggedEntries.forEach((entry) => {
-  const flaggedJournal = document.createElement("div");
-  flaggedJournal.classList = "flagged-header";
-  flaggedJournal.textContent = `${entry.date} // ${entry.journal}`;
+cardAccordion.forEach((card) => {
+  let content = card.querySelector(".card-content");
+  let header = card.querySelector(".card-title");
 
-  const flaggedOther = document.createElement("div");
-  flaggedOther.classList = "flagged-content";
-  flaggedOther.style.display = "none";
+  content.style.display = "none";
 
-  // Emotion Tracker Results
-  let selectedEmotion;
-
-  if (entry.emotionTracker === "one") {
-    selectedEmotion = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-disgusting-100.png";
-    selectedEmotion.appendChild(resultImg);
-  } else if (entry.emotionTracker === "two") {
-    selectedEmotion = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-nauseated-face-100.png";
-    selectedEmotion.appendChild(resultImg);
-  } else if (entry.emotionTracker === "three") {
-    selectedEmotion = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-pouting-face-100.png";
-    selectedEmotion.appendChild(resultImg);
-  } else if (entry.emotionTracker === "four") {
-    selectedEmotion = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-blushing-100.png";
-    selectedEmotion.appendChild(resultImg);
-  } else if (entry.emotionTracker === "five") {
-    selectedEmotion = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src =
-      "./icons_images/icons8-grinning-face-with-smiling-eyes-100.png";
-    selectedEmotion.appendChild(resultImg);
-  } else {
-    selectedEmotion = document.createElement("div");
-    selectedEmotion = "No emotion documented.";
-  }
-
-  // Water Tracker Results
-  let selectedWater;
-
-  if (entry.waterTracker === "one") {
-    selectedWater = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-water-0.png";
-    selectedWater.appendChild(resultImg);
-  } else if (entry.waterTracker === "two") {
-    selectedWater = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-water-8.png";
-    selectedWater.appendChild(resultImg);
-  } else if (entry.waterTracker === "three") {
-    selectedWater = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-water-16.png";
-    selectedWater.appendChild(resultImg);
-  } else if (entry.waterTracker === "four") {
-    selectedWater = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-water-24.png";
-    selectedWater.appendChild(resultImg);
-  } else if (entry.waterTracker === "five") {
-    selectedWater = document.createElement("div");
-    let resultImg = document.createElement("img");
-    resultImg.src = "./icons_images/icons8-water-32.png";
-    selectedWater.appendChild(resultImg);
-  } else {
-    selectedWater = document.createElement("div");
-    selectedWater = "No water documented.";
-  }
-
-  // Weather Results
-  const weather = entry.weather;
-  const weatherDiv = document.createElement("div");
-  weatherDiv.textContent = weather;
-
-  // Medication Results
-  const medications = entry.medications;
-  const medDiv = document.createElement("div");
-  medications.forEach((medication) => {
-    const savedMedText = document.createElement("p");
-    savedMedText.textContent = `Medication: ${medication.MedText}, Count: ${medication.MedCount}, Checked: ${medication.IsChecked}`;
-    medDiv.appendChild(savedMedText);
-  });
-
-  // Exercises Results
-  const exercises = entry.exercises;
-  const exDiv = document.createElement("div");
-  exercises.forEach((exercise) => {
-    const savedExText = document.createElement("p");
-    savedExText.textContent = `Exercise: ${exercise.Exercise}, Reps: ${exercise.RepCount}, Checked: ${exercise.IsChecked}`;
-    exDiv.appendChild(savedExText);
-  });
-
-  // Habits Results
-  const habits = entry.habits;
-  const habDiv = document.createElement("div");
-  habits.forEach((habit) => {
-    const savedHabText = document.createElement("p");
-    savedHabText.textContent = `Habit: ${habit.Habit}, Checked: ${habit.IsChecked}`;
-    habDiv.appendChild(savedHabText);
-  });
-
-  // Appending All Results to flaggedOther
-  flaggedOther.append(selectedEmotion);
-  flaggedOther.append(selectedWater);
-  flaggedOther.append(weatherDiv);
-  flaggedOther.append(medDiv);
-  flaggedOther.append(exDiv);
-  flaggedOther.append(habDiv);
-
-  flaggedJournal.append(flaggedOther);
-  container.appendChild(flaggedJournal);
-
-  flaggedJournal.addEventListener("click", (event) => {
+  header.addEventListener("click", (event) => {
     if (!event.target.closest("button, a, input")) {
-      flaggedOther.style.display =
-        flaggedOther.style.display === "block" ? "none" : "block";
+      content.style.display =
+        content.style.display === "block" ? "none" : "block";
     }
   });
 });
 
-console.log(flaggedEntries);
+function createEmotionElement(emotionTracker) {
+  const selectedEmotion = document.createElement("div");
+  let emotionImg = document.createElement("img");
+
+  const emotionMap = {
+    one: "./icons_images/icons8-disgusting-100.png",
+    two: "./icons_images/icons8-nauseated-face-100.png",
+    three: "./icons_images/icons8-pouting-face-100.png",
+    four: "./icons_images/icons8-blushing-100.png",
+    five: "./icons_images/icons8-grinning-face-with-smiling-eyes-100.png",
+  };
+
+  const imgSrc = emotionMap[emotionTracker];
+
+  if (imgSrc) {
+    emotionImg.src = imgSrc;
+    selectedEmotion.appendChild(emotionImg);
+  } else {
+    selectedEmotion.textContent = "No emotion selected.";
+  }
+  return selectedEmotion;
+}
+
+function createWaterElement(waterTracker) {
+  const selectedWater = document.createElement("div");
+  let waterImg = document.createElement("img");
+
+  const waterMap = {
+    one: "./icons_images/icons8-water-0.png",
+    two: "./icons_images/icons8-water-8.png",
+    three: "./icons_images/icons8-water-16.png",
+    four: "./icons_images/icons8-water-24.png",
+    five: "./icons_images/icons8-water-32.png",
+  };
+
+  const imgSrc = waterMap[waterTracker];
+
+  if (imgSrc) {
+    waterImg.src = imgSrc;
+    selectedWater.appendChild(waterImg);
+  } else {
+    selectedWater.textContent = "Water intake not tracked.";
+  }
+  return selectedWater;
+}
+
+function createMedicationsElement(medications) {
+  const medDiv = document.createElement("div");
+
+  medications.forEach((medication) => {
+    const medicationElement = document.createElement("div");
+    medicationElement.textContent = `Medication: ${medication.MedText}, Count: ${medication.MedCount}, Checked: ${medication.IsChecked}`;
+
+    medDiv.append(medicationElement);
+  });
+  return medDiv;
+}
+
+function createExercisesElement(exercises) {
+  const exDiv = document.createElement("div");
+
+  exercises.forEach((exercise) => {
+    const exerciseElement = document.createElement("div");
+    exerciseElement.textContent = `Exercise: ${exercise.Exercise}, Reps: ${exercise.RepCount}, Checked: ${exercise.IsChecked}`;
+
+    exDiv.append(exerciseElement);
+  });
+  return exDiv;
+}
+
+function createHabitsElement(habits) {
+  const habDiv = document.createElement("div");
+
+  habits.forEach((habit) => {
+    const habitElement = document.createElement("div");
+    habitElement.textContent = `Habit: ${habit.Habit}, Checked: ${habit.IsChecked}`;
+
+    habDiv.append(habitElement);
+  });
+  return habDiv;
+}
+
+function displayCards() {
+  const cardsContainer = document.getElementById("flaggedItemsContainer");
+  cardsContainer.innerHTML = "";
+
+  flaggedEntries.forEach((entry) => {
+    const weather = entry.weather;
+    let emotionTracker = entry.emotionTracker;
+    let waterTracker = entry.waterTracker;
+    let medications = entry.medications;
+    let exercises = entry.exercises;
+    let habits = entry.habits;
+
+    let selectedEmotion = createEmotionElement(emotionTracker);
+    let selectedWater = createWaterElement(waterTracker);
+    let medicationList = createMedicationsElement(medications);
+    let exerciseList = createExercisesElement(exercises);
+    let habitsList = createHabitsElement(habits);
+
+    let cardContent =
+      weather +
+      selectedEmotion.outerHTML +
+      selectedWater.outerHTML +
+      medicationList.outerHTML +
+      exerciseList.outerHTML +
+      habitsList.outerHTML;
+
+    const card = document.createElement("div");
+    card.classList.add("card", "w-50", "p-3");
+
+    const cardJournal = document.createElement("p");
+    cardJournal.classList.add("card-journal");
+    cardJournal.textContent = entry.journal;
+
+    const cardAccordion = document.createElement("div");
+    cardAccordion.classList.add("card-accordion");
+
+    const cardContentDiv = document.createElement("div");
+    cardContentDiv.classList.add("card-content");
+    cardContentDiv.innerHTML = cardContent;
+
+    const cardBodyTitle = document.createElement("div");
+    cardBodyTitle.classList.add("card-body");
+
+    const cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.textContent = entry.date;
+
+    cardAccordion.appendChild(cardContentDiv);
+    cardBodyTitle.appendChild(cardTitle);
+
+    card.appendChild(cardJournal);
+    card.appendChild(cardAccordion);
+    card.appendChild(cardBodyTitle);
+
+    cardsContainer.appendChild(card);
+  });
+}
+
+displayCards();
